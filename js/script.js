f54100cd6118a49b8e24b0cb865a0d5893709108
Mikhail Karpoyan <49282278+asmadeusx@users.noreply.github.com>
2021-03-08 18:08:56 +0300

    четвертый блок закончил
    четвертый блок закончил

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const brandList = [
+    'Robe', 
+    'SGM', 
+    'StudioDue', 
+    'ChainMaster', 
+    'Milos', 
+    'Universal Effects', 
+    'ArtLighting', 
+    'MobilTech', 
+    'Lampo', 
+    'Martin', 
+    'Spotlight', 
+    'Compulite', 
+    'Eurolite'
+];
+
 const fs = document.querySelector('.first-screen');
 const fsBg = document.querySelector('.first-screen__bg');
 
