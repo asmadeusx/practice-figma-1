d06d7c738f4f2031004f331a7b3196640830056c
Mikhail <Virus1231@yandex.ru>
2021-03-11 12:57:41 +0300

    screen project - SLIDER - DONE
    screen project - SLIDER - DONE

--- js/script.js
+++ js/script.js
@@ -1,9 +1,59 @@
 'use strict';
+
 //#region SCREEN - 1 - TITLE
 
 //#endregion
 
-//#region SCREEN - 2 - SLIDER
+//#region SCREEN - 2 - SLIDER - DONE
+
+const s2sliderbtns = document.querySelectorAll('.sliderbtns__btn');
+const s2sliders = document.querySelectorAll('.list__slide');
+const s2playpausebtn = document.getElementById('playpause');
+const s2btnnext = document.querySelector('.next');
+const s2btnprev = document.querySelector('.prev');
+const s2intervalMs = 7000;
+let s2SlideInterval = setInterval(nextSlide, s2intervalMs);
+let s2currentSlide = 0;
+
+function nextSlide() {
+    slider(s2currentSlide + 1);
+}
+
+function prevSlide() {
+    slider(s2currentSlide - 1);
+}
+
+function slider(n) {
+    hide(s2sliders[s2currentSlide]);
+    s2currentSlide = (n + s2sliders.length) % s2sliders.length;
+    show(s2sliders[s2currentSlide]);
+}
+
+s2btnnext.addEventListener('click', nextSlide);
+s2btnprev.addEventListener('click', prevSlide);
+
+s2playpausebtn.addEventListener('click', () => {
+    console.log(s2playpausebtn);
+    if (s2playpausebtn.classList.contains('pause')) {
+        s2playpausebtn.innerHTML = '<i class="fas fa-play"></i>';
+        s2playpausebtn.classList.remove('pause');
+        s2playpausebtn.classList.add('play');
+        clearInterval(s2SlideInterval);
+    } else {
+        s2playpausebtn.innerHTML = '<i class="fas fa-pause"></i>';
+        s2playpausebtn.classList.remove('play');
+        s2playpausebtn.classList.add('pause');
+        s2SlideInterval = setInterval(nextSlide, s2intervalMs);
+    }
+});
+
+function hide(item) {
+    item.classList.remove('show');
+}
+
+function show(item) {
+    item.classList.add('show');
+}
 
 //#endregion
 
